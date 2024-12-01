0c2e1121b80d7afb54c80cc94e3da4f59dd45f54
rl-86 <rl86@live.se>
2024-12-01 13:47:30 +0100

    Initial commit for Lab2 Spring Boot project

---
+++ src/main/java/org/example/lab2_springboot/Lab2SpringbootApplication.java
@@ -0,0 +1,13 @@
+package org.example.lab2_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab2SpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab2SpringbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/lab2_springboot/Lab2SpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.lab2_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab2SpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
